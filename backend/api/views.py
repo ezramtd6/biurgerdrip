05e509180548c5eb476375f03f891a70e7b5b22d
ezramtd5 <ezramekuria463@gmail.com>
2026-07-25 06:50:17 +0300

    Initial commit

---
+++ backend/api/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+# api/views.py
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(["GET"])
+def hello(request):
+    return Response({
+        "message": "Hello from Ezraa!"
+    })
